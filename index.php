63249fd821cc328b601254006fda689e148c3bba
aldkgithub <79628750+aldkgithub@users.noreply.github.com>
2021-10-16 15:15:38 +0500

    Create index.php
    Main file for multiple Dice Roller.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Dice Roller App </title>
+	</head>
+	<body>
+		<h1>Dice Roller App</h1>
+		<input type="number" id="dicecount" name="dicecount" min="1" max="20">
+		<br>
+		<select name="dicesides">
+			<option> 02 Sided </option>
+			<option> 04 Sided </option>
+			<option> 08 Sided </option>
+			<option> 10 Sided </option>
+			<option> 12 Sided </option>
+			<option> 20 Sided </option>
+		</select>
+	</body>
+
+</html>
